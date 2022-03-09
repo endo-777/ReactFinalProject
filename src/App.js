82a5e2a8f234d2113e5a3defe4dcd77ac86f7851
c0zyb0y <no.phupha_st@tni.ac.th>
2022-03-09 19:07:31 +0700

    Finish up switch light-dark-theme

--- src/App.js
+++ src/App.js
@@ -7,14 +7,50 @@ import ResumePage from "./Pages/ResumePage";
 import ProductPage from "./Pages/ProductPage";
 import CartPage from "./Pages/CartPage";
 import ContactPage from "./Pages/ContactPage";
-
+import Switch from "@material-ui/core/Switch";
+import { IconButton } from "@material-ui/core"
+import { Brightness4 } from "@material-ui/icons";
+import { useEffect, useState } from "react";
 
 
 function App() {
+
+  const [theme, setTheme] = useState('dark-theme')
+  const [checked, setChecked] = useState(false)
+
+  const themeToggler = () => {
+    if(theme === 'light-theme'){
+      setTheme('dark-theme')
+      setChecked(false)
+    }else{
+      setTheme('light-theme')
+      setChecked(true)
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  },[theme])
+
+
   return (
     <div className="App">
       <Sidebar/>
       <MainContentStyled className="main-content">
+        <div className="light-dark-mode">
+          <div className="left-content">
+            <Brightness4/>
+          </div>
+          <div className="right-content">
+          <Switch 
+            value=""
+            checked={checked}
+            inputProps={{'arial-label':''}}
+            size="medium"
+            onClick={themeToggler}
+            />
+          </div>
+        </div>
         <Switching>
           <Route path='/' exact> <HomePage/> </Route>
           <Route path='/about'> <AboutPage/> </Route>
--- src/styled/GlobalStyle.js
+++ src/styled/GlobalStyle.js
@@ -2,7 +2,44 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyled = createGlobalStyle`
 
-:root{
+// :root{
+//     --primary-color: #007bff;
+//     --primary-color-light: #057FFF;
+//     --secondary-color: #6c757d;
+//     --background-dark-color: #10121A;
+//     --background-dark-grey: #191D2B;
+//     --border-color: #2e344e;
+//     --background-light-color: #F1F1F1;
+//     --background-light-color-2: rgba(3,127,255,.3);
+//     --white-color: #FFF;
+//     --font-light-color: #a4acc4;
+//     --font-dark-color: #313131;
+//     --font-dark-color-2: #151515;
+//     --sidebar-dark-color: #191D2B;
+//     --scrollbar-bg-color: #383838;
+//     --scrollbar-thump-color: #6b6b6b;
+//     --scrollbar-track-color: #383838;
+
+// }
+.light-theme{
+    --primary-color: #007bff;
+    --primary-color-light: #057FFF;
+    --secondary-color: #ff7675;
+    --background-dark-color: #F1F1F1;
+    --background-dark-grey: #e4e4e4;
+    --border-color: #cbced8;
+    --background-light-color: #F1F1F1;
+    --background-light-color-2: rgba(3,127,255,.3);
+    --white-color: #151515;
+    --font-light-color: #313131;
+    --font-dark-color: #313131;
+    --font-dark-color-2: #151515;
+    --sidebar-dark-color: #E4E4E4;
+    --scrollbar-bg-color: #383838;
+    --scrollbar-thump-color: #6b6b6b;
+    --scrollbar-track-color: #383838;
+}
+.dark-theme{
     --primary-color: #007bff;
     --primary-color-light: #057FFF;
     --secondary-color: #6c757d;
@@ -19,7 +56,6 @@ const GlobalStyled = createGlobalStyle`
     --scrollbar-bg-color: #383838;
     --scrollbar-thump-color: #6b6b6b;
     --scrollbar-track-color: #383838;
-
 }
 
 *{
@@ -53,7 +89,24 @@ h1{
 span{
     color: var(--primary-color);
 }
-
+.light-dark-mode{
+    position: fixed;
+    right: 0;
+    top: 50%;
+    background-color: var(--background-light-color-2);
+    width: 6.5rem;
+    height: 2.5rem;
+    z-index: 15;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg{
+      display: flex;
+      align-items: center;
+      font-size: 1.7rem;
+      color: var(--white-color);
+    }
+  }
 `;
 
 export default GlobalStyled;
